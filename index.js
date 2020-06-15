3195ec69035809375fefcf9939f6bde691582a65
Jaeson Booker <jaesonbooker@gmail.com>
2020-06-14 23:03:31 -0700

    cleaning up

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let request = require('request');
 const path = require('path');
 const {spawn} = require('child_process');
-// const ls = spawn('ls', ['-lh', '/usr']);
 const node = await IPFS.create()
 let handle = (data, callback) => {
     var results;
@@ -15,11 +14,10 @@ let handle = (data, callback) => {
     for await (const chunk of test_y_stream) {
       test_y_model += chunk
     }
-    // let url = "https://ipfs.infura.io:5001/api/v0/object/stat?arg=";
-    for i in range(0, len(data.training_model_hash)):
-      //url = url + data[i]
+
+    for i in range(0, len(data.training_hash_array)):
       var training_model;
-      const training_stream = node.cat(data.training_model_hash[i]);
+      const training_stream = node.cat(data.training_hash_array[i]);
 
       for await (const chunk of training_stream) {
         training_model += chunk
@@ -40,91 +38,18 @@ let handle = (data, callback) => {
         results.append(code);
         console.log(results)
       });
-    // let requestObj;
-
-    // switch (data.endpoint) {
-    //     case "price":
-    //         requestObj = {
-    //             fsym: data.fsym,
-    //             tsyms: data.tsyms
-    //         };
-    //         break;
-    //     case "pricemulti":
-    //     case "pricemultifull":
-    //         requestObj = {
-    //             fsyms: data.fsyms,
-    //             tsyms: data.tsyms
-    //         };
-    //         break;
-    //     case "generateAvg":
-    //         requestObj = {
-    //             fsym: data.fsym,
-    //             tsym: data.tsym,
-    //             e: data.exchange
-    //         };
-    //         break;
-    //     default:
-    //         requestObj = {
-    //             fsym: data.fsym,
-    //             tsyms: data.tsyms
-    //         };
-    //         break;
-    // }
-
-    // let options = {
-    //     url: url,
-    //     qs: requestObj,
-    //     json: true
-    // };
-
-    // request(options, (error, response, body) => {
-    //     if (error || response.statusCode >= 400) {
-    //         callback(response.statusCode, {
-    //             jobRunID: data.id,
-    //             status: "errored",
-    //             error: error
-    //         });
-    //     } else {
-    //         let resp = body;
-    //         if (data.endpoint === "price")
-    //             resp.result = resp[data.tsyms];
-    //
-    //         callback(response.statusCode, {
-    //             jobRunID: data.id,
-    //             data: body
-    //         });
-    //     }
-    // });
 };
 
-exports.handler = (event, context, callback) => {
+exports.handler = (event, callback) => {
     let data = {
         id: event.id,
         endpoint: event.data.endpoint || "",
-        fsyms: event.data.fsyms || "",
-        fsym: event.data.coin || event.data.fsym || "",
-        tsyms: event.data.market || event.data.tsyms || "",
-        tsym: event.data.tsym || "",
-        exchange: event.data.exchange || ""
+        test_x_hash: event.test_x_hash,
+        test_y_hash: event.test_y_hash,
+        training_hash_array: event.training_hash_array
     };
 
     handle(data, (statusCode, responseData) => {
         callback(null, responseData);
     });
 };
-
-// exports.gcpservice = (req, res) => {
-//     let data = {
-//         id: req.body.id,
-//         endpoint: req.body.data.endpoint || "price",
-//         fsyms: req.body.data.fsyms || "",
-//         fsym: req.body.data.coin || req.body.data.fsym || "",
-//         tsyms: req.body.data.market || req.body.data.tsyms || "",
-//         tsym: req.body.data.tsym || "",
-//         exchange: req.body.data.exchange || ""
-//     };
-//
-//     handle(data, (statusCode, responseData) => {
-//         res.status(statusCode).send(responseData);
-//     });
-// };
--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-    adaptor.gcpservice(req, res)
+    adaptor.handler(req, res)
 });
 
 let listener = app.listen(process.env.PORT, function () {
-    console.log("CryptoCompare External Adaptor listening on", listener.address().address + listener.address().port);
+    console.log("Tensorfloracle External Adaptor listening on", listener.address().address + listener.address().port);
 });
